48f5681eb723f1cbd2745fcd2f696e4d749967ba
unknown <daffatabianoo@gmail.com>
2024-11-19 15:21:57 +0700

    feat(components): Navbar components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+import flowbite from 'flowbite-react/tailwind';
+
+export const content = [
+  './index.html',
+  './src/**/*.{js,jsx,ts,tsx}',
+  flowbite.content(),
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [flowbite.content()];

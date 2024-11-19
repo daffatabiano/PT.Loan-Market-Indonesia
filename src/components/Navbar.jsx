e5194109590d1240e2852f4cfed1c3ed34a28641
unknown <daffatabianoo@gmail.com>
2024-11-19 15:31:48 +0700

    feat(components): navbar components added.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,31 @@
+import { Button, Popover } from 'flowbite-react';
+import { useState } from 'react';
+import { CiBellOn } from 'react-icons/ci';
+import { IoChevronDown } from 'react-icons/io5';
+
 export default function Navbar() {
+  const [bellOpen, setBellOpen] = useState(false);
+
   return (
     <nav className="bg-white p-4 flex justify-between items-center shadow-md">
       <h1 className="text-[#4F5051] text-2xl font-thin ">Dashboard</h1>
       <div className="flex items-center gap-2">
-        <Dropdown></Dropdown>
+        <Popover
+          aria-labelledby="simple-popover"
+          open={bellOpen}
+          onOpenChange={setBellOpen}
+          content={
+            <div className="w-full flex place-items-center">
+              <h1 className="text-md font-thin text-slate-400">
+                This is Notifications example ui
+              </h1>
+            </div>
+          }>
+          <Button>
+            <CiBellOn />
+            <IoChevronDown />
+          </Button>
+        </Popover>
       </div>
     </nav>
   );

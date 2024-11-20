1f4a2a09f0060d39c9e7b231adb01db3521eb22d
unknown <daffatabianoo@gmail.com>
2024-11-20 08:00:45 +0700

    fix(nav): add headers in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Button, Popover } from 'flowbite-react';
+import { Popover } from 'flowbite-react';
 import { useState } from 'react';
 import { BsBuildings } from 'react-icons/bs';
 import { CiBellOn, CiMail } from 'react-icons/ci';
@@ -8,12 +8,15 @@ import { FiPhone } from 'react-icons/fi';
 
 export default function Navbar() {
   const [bellOpen, setBellOpen] = useState(false);
+  const [profileOpen, setProfileOpen] = useState(false);
 
   return (
     <nav className="w-full">
       <div className="bg-sidebar border-b-2 ps-[22%] border-slate-800 sticky top-0 left-0 right-0 w-full p-4 h-20 flex justify-between items-center shadow-md">
         <h1 className="text-[#4F5051] text-2xl w-fit font-thin ">Dashboard</h1>
         <div className="flex items-center gap-2">
+          <hr className="w-[1px] h-10 bg-[#E2F0FF]" />
+
           <Popover
             aria-labelledby="simple-popover"
             open={bellOpen}
@@ -21,36 +24,55 @@ export default function Navbar() {
             className="flex items-center gap-2 bg-transparent text-xl text-black hover:bg-transparent focus:bg-transparent hover:border-0"
             content={
               <div className="w-full flex place-items-center">
-                <h1 className="text-sm text-center font-thin text-slate-400 w-32 h-44 bg-white flex place-items-center dropshadow-md p-2">
+                <h1 className="text-sm text-center font-thin text-[#4F5051] w-32 h-44 bg-white flex place-items-center dropshadow-md p-2">
                   This is Notifications example ui
                 </h1>
               </div>
             }>
-            <Button className="bg-transparent hover:bg-none text-black">
-              <CiBellOn />
-              <IoChevronDown />
-            </Button>
+            <p className="bg-transparent flex items-center hover:bg-none text-black">
+              <CiBellOn className="text-2xl" />
+              <IoChevronDown className="text-2xl" />
+            </p>
           </Popover>
-          <span className="text-[#646567] text-lg uppercase">
-            Yohannes affandy
-          </span>
+          <hr className="w-[1px] h-10 bg-[#E2F0FF]" />
+          <Popover
+            aria-labelledby="simple-popover"
+            open={profileOpen}
+            onOpenChange={setProfileOpen}
+            className="flex items-center gap-2 bg-transparent text-xl text-black hover:bg-transparent focus:bg-transparent hover:border-0"
+            content={
+              <div className="w-full flex place-items-center">
+                <h1 className="text-sm text-center font-thin text-[#4F5051] w-32 h-44 bg-white flex place-items-center dropshadow-md p-2">
+                  This is Notifications example ui
+                </h1>
+              </div>
+            }>
+            <p className="text-[#646567] text-lg uppercase flex gap-2 items-center">
+              Yohannes affandy <IoChevronDown className="text-2xl" />
+            </p>
+          </Popover>
+
+          <hr className="w-[1px] h-10 bg-[#E2F0FF]" />
         </div>
       </div>
+      <Header />
     </nav>
   );
 }
 
 export function Header() {
   return (
-    <header className="flex p-8 sticky top-0 left-0 right-0 w-full items-center gap-4 justify-start">
+    <header className="flex ps-[22%] bg-white py-4 sticky top-0 left-0 right-0 w-full items-center gap-4 justify-start">
       <img
         src={ProfilePicture}
         alt="profile"
         className="w-24 h-24 rounded-full"
       />
       <div>
-        <h1>Yohannes Affandy</h1>
-        <div className="flex gap-4">
+        <h1 className="text-2xl font-bold text-primary uppercase tracking-wide">
+          Yohannes Affandy (JOJO)
+        </h1>
+        <div className="flex gap-4 pt-4">
           <div className="flex flex-col gap-2">
             <InformationsField
               icon={<BsBuildings />}
@@ -58,7 +80,7 @@ export function Header() {
             />
             <InformationsField icon={'ID'} text="LM9990001" />
           </div>
-          <hr className="w-[1px] h-10 bg-[#E2F0FF]" />
+          <hr className="w-[1px] h-24 bg-[#E2F0FF]" />
           <div className="flex flex-col gap-2">
             <InformationsField icon={<CiMail />} text="it@loanmarket.co.id" />
             <InformationsField icon={<FiPhone />} text="6281234567890" />
@@ -72,7 +94,7 @@ export function Header() {
 function InformationsField(prop) {
   const { icon, text } = prop;
   return (
-    <p className="flex items-center text-black">
+    <p className="flex items-center text-black gap-2 font-semibold">
       <span className="p-2 rounded-full bg-[#E2F0FF] text-primary">{icon}</span>
       {text}
     </p>
